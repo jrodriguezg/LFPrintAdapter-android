f78c1b2cb72d1f6c91deed9a30529543aa4c6c70
Juan Manuel Rodriguez Garcia <juan-manuel.rodriguez-garcia@hp.com>
2016-06-03 15:55:42 +0200

    Improve FileListAdapterTest to remove ActivityInstrumentationTestCase2 dependency.

--- app/src/androidTest/java/com/jmrodrigg/printing/FileListAdapterTest.java
+++ app/src/androidTest/java/com/jmrodrigg/printing/FileListAdapterTest.java
@@ -1,10 +1,14 @@
 package com.jmrodrigg.printing;
 
+import com.jmrodrigg.printing.controller.FileListAdapter;
+
 import org.junit.Before;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
-import android.test.ActivityInstrumentationTestCase2;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.test.AndroidTestCase;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -18,34 +22,34 @@ import static org.junit.Assert.assertThat;
  * Author: jrodriguezg
  * Date: 30/05/16.
  */
-public class FileListAdapterTest extends ActivityInstrumentationTestCase2<FilePickerActivity> {
+public class FileListAdapterTest extends AndroidTestCase {
 
-    FilePickerActivity.FileListAdapter mFileListAdapter;
-    @Mock
-    ArrayList mFiles;
+    FileListAdapter mFileListAdapter;
+
+    ArrayList<File> mFiles;
     @Mock
     File aFile;
 
-    public FileListAdapterTest() {
-        super(FilePickerActivity.class);
-    }
-
     @Before
-    public void setUp() {
-        System.setProperty(
-                "dexmaker.dexcache",
-                getInstrumentation().getTargetContext().getCacheDir().getPath());
+    public void setUp() throws Exception{
+        super.setUp();
 
-        mFiles = Mockito.mock(ArrayList.class);
+        Context context = InstrumentationRegistry.getTargetContext();
+        setContext(context);
+        assertNotNull(context);
+
         aFile = Mockito.mock(File.class);
-        mFileListAdapter = getActivity().new FileListAdapter(getActivity().getBaseContext(),R.layout.file_item,mFiles);
+        Mockito.when(aFile.isDirectory()).thenReturn(false);
+
+        mFiles = new ArrayList<>();
+        mFiles.add(aFile);
+
+        mFileListAdapter = new FileListAdapter(getContext(), R.layout.file_item, mFiles);
     }
 
     public void testListItem_Image_Valid() {
         Mockito.when(aFile.getName()).thenReturn("validFile.jpeg");
         Mockito.when(aFile.isDirectory()).thenReturn(false);
-
-        Mockito.when(mFiles.get(0)).thenReturn(aFile);
 
         View view = mFileListAdapter.getView(0,null,null);
 
@@ -53,7 +57,18 @@ public class FileListAdapterTest extends ActivityInstrumentationTestCase2<FilePi
 
         ImageView imgIcon = (ImageView) view.findViewById(R.id.ImgViewFiletype);
         assertTrue(imgIcon.getDrawable().getConstantState().equals
-                (getActivity().getBaseContext().getResources().getDrawable(R.drawable.type_jpg).getConstantState()));
+                (getContext().getDrawable(R.drawable.type_jpg).getConstantState()));
+
+        Mockito.when(aFile.getName()).thenReturn("validFile.jpg");
+        Mockito.when(aFile.isDirectory()).thenReturn(false);
+
+        view = mFileListAdapter.getView(0,null,null);
+
+        assertThat(view, instanceOf(view.getClass()));
+
+        imgIcon = (ImageView) view.findViewById(R.id.ImgViewFiletype);
+        assertTrue(imgIcon.getDrawable().getConstantState().equals
+                (getContext().getDrawable(R.drawable.type_jpg).getConstantState()));
 
 
         Mockito.when(aFile.getName()).thenReturn("validFile.png");
@@ -63,34 +78,42 @@ public class FileListAdapterTest extends ActivityInstrumentationTestCase2<FilePi
 
         imgIcon = (ImageView) view.findViewById(R.id.ImgViewFiletype);
         assertTrue(imgIcon.getDrawable().getConstantState().equals
-                (getActivity().getBaseContext().getResources().getDrawable(R.drawable.type_png).getConstantState()));
+                (getContext().getDrawable(R.drawable.type_png).getConstantState()));
 
     }
 
     public void testListItem_Document_Valid() {
         Mockito.when(aFile.getName()).thenReturn("validFile.pdf");
-        Mockito.when(aFile.isDirectory()).thenReturn(false);
-
-        Mockito.when(mFiles.get(0)).thenReturn(aFile);
 
         View view = mFileListAdapter.getView(0,null,null);
         assertThat(view, instanceOf(view.getClass()));
 
         ImageView imgIcon = (ImageView) view.findViewById(R.id.ImgViewFiletype);
         assertTrue(imgIcon.getDrawable().getConstantState().equals
-                (getActivity().getBaseContext().getResources().getDrawable(R.drawable.type_pdf).getConstantState()));
+                (getContext().getDrawable(R.drawable.type_pdf).getConstantState()));
     }
 
     public void testListItem_Folder_Valid() {
         Mockito.when(aFile.isDirectory()).thenReturn(true);
 
-        Mockito.when(mFiles.get(0)).thenReturn(aFile);
-
         View view = mFileListAdapter.getView(0,null,null);
         assertThat(view, instanceOf(view.getClass()));
 
         ImageView imgIcon = (ImageView) view.findViewById(R.id.ImgViewFiletype);
         assertTrue(imgIcon.getDrawable().getConstantState().equals
-                (getActivity().getBaseContext().getResources().getDrawable(R.drawable.type_folder).getConstantState()));
+                (getContext().getDrawable(R.drawable.type_folder).getConstantState()));
+    }
+
+    public void testListItem_Unknown_Valid() {
+        Mockito.when(aFile.getName()).thenReturn("unknown.txt");
+        Mockito.when(aFile.isDirectory()).thenReturn(false);
+
+        View view = mFileListAdapter.getView(0,null,null);
+        assertThat(view, instanceOf(view.getClass()));
+
+        ImageView imgIcon = (ImageView) view.findViewById(R.id.ImgViewFiletype);
+
+        assertTrue(imgIcon.getDrawable().getConstantState().equals
+                (getContext().getDrawable(R.drawable.type_generic).getConstantState()));
     }
 }
--- app/src/main/java/com/jmrodrigg/printing/FilePickerActivity.java
+++ app/src/main/java/com/jmrodrigg/printing/FilePickerActivity.java
@@ -1,12 +1,12 @@
 package com.jmrodrigg.printing;
 
 import com.jmrodrigg.printing.controller.FileList;
+import com.jmrodrigg.printing.controller.FileListAdapter;
 import com.jmrodrigg.printing.model.PrintJob;
 import com.jmrodrigg.printing.samples.PrintCustomContent;
 
 import android.Manifest;
 import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -14,22 +14,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 
 /**
  * Author: jrodriguezg
@@ -138,83 +129,6 @@ public class FilePickerActivity extends ListActivity {
     private void fillList() {
         this.setTitle(mFileListController.fillList());
         this.setListAdapter(new FileListAdapter(getBaseContext(), R.layout.file_item, mFileListController));
-    }
-
-    class FileListAdapter extends ArrayAdapter<File> {
-
-        Context mContext;
-        int mResourceId;
-        List<File> mObjects;
-
-        public FileListAdapter(Context context, int resource, List<File> objects) {
-            super(context, resource, objects);
-
-            mContext = context;
-            mResourceId = resource;
-            mObjects = objects;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            ImageView imgIcon;
-            TextView txtName, txtSize, txtDate;
-
-            if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(mResourceId, null);
-            }
-
-            File file = mObjects.get(position);
-            if (file != null) {
-                imgIcon = (ImageView) convertView.findViewById(R.id.ImgViewFiletype);
-                txtName = (TextView) convertView.findViewById(R.id.txtFileName);
-                txtSize = (TextView) convertView.findViewById(R.id.txtSize);
-                txtDate = (TextView) convertView.findViewById(R.id.txtDate);
-
-                int resource = R.drawable.type_generic;
-
-                String fileName = file.getName();
-                long size = file.length();
-                long lastModified = file.lastModified();
-
-                if (file.isDirectory()) {
-                    resource = R.drawable.type_folder;
-                    txtSize.setText("");
-                } else {
-                    // Determine mime type:
-                    if (fileName.toUpperCase().endsWith(".PDF")) {
-                        // PDF:
-                        resource = R.drawable.type_pdf;
-                    } else if (fileName.toUpperCase().endsWith(".JPG") || file.getName().toUpperCase().endsWith(".JPEG")) {
-                        // JPG:
-                        resource = R.drawable.type_jpg;
-                    } else if (fileName.toUpperCase().endsWith(".PNG")) {
-                        // PNG:
-                        resource = R.drawable.type_png;
-                    }
-
-                    // If file, set size:
-                    if (size <= 1024)
-                        txtSize.setText(getResources().getString(R.string.file_size_bytes, size));
-                    else if (size <= (1024 * 1024))
-                        txtSize.setText(getResources().getString(R.string.file_size_Kbytes, String.format(Locale.getDefault(), "%.2f", (float) size / 1024)));
-                    else
-                        txtSize.setText(getResources().getString(R.string.file_size_Mbytes, String.format(Locale.getDefault(), "%.2f", (float) size / (1024 * 1024))));
-                }
-
-                // Set Icon:
-                imgIcon.setImageResource(resource);
-                // Set name:
-                txtName.setText(fileName);
-                // Set Last Modified date:
-                SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy - hh:mm", Locale.getDefault());
-                txtDate.setText(sd.format(new Date(lastModified)));
-
-            }
-
-            return convertView;
-        }
     }
 
 }
