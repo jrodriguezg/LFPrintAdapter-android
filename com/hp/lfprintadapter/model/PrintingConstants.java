c05c0a97f90f30687fddbd964ed6091b8349296f
Juan Manuel Rodriguez Garcia <juan-manuel.rodriguez-garcia@hp.com>
2016-10-03 12:09:05 +0200

    Initial commit.

---
+++ com/hp/lfprintadapter/model/PrintingConstants.java
@@ -0,0 +1,15 @@
+package com.hp.lfprintadapter.model;
+
+/**
+ * Author: jrodriguezg
+ * Date: 03/05/2016.
+ */
+public class PrintingConstants {
+
+    // Printing settings:
+    public enum FitMode { PRINT_FIT_TO_PAGE,  PRINT_CLIP_CONTENT, PASS_PDF_AS_IS }
+    enum JobType { DOCUMENT }
+
+    // In order to be able to save a copy of the print job in the device, permissions must be granted:
+    public static final boolean DUMP_FILE = true;
+}
