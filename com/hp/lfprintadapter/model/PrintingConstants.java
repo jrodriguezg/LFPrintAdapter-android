372821621eaca31f661c7a20522ab19706facde5
jrodriguezg <johntherip@gmail.com>
2016-10-05 23:43:57 +0200

    Make JobType public so clients can access it when calling PrintJob.setMimeType();

--- com/hp/lfprintadapter/model/PrintingConstants.java
+++ com/hp/lfprintadapter/model/PrintingConstants.java
@@ -8,7 +8,7 @@ public class PrintingConstants {
 
     // Printing settings:
     public enum FitMode { PRINT_FIT_TO_PAGE,  PRINT_CLIP_CONTENT, PASS_PDF_AS_IS }
-    enum JobType { DOCUMENT }
+    public enum JobType { DOCUMENT }
 
     // In order to be able to save a copy of the print job in the device, permissions must be granted:
     public static final boolean DUMP_FILE = true;
